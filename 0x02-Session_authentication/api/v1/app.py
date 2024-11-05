9d4819c86988ba2583ea52e6816184b41aa3d520
El Mehdi <azghoulmehdi899@gmail.com>
2024-11-05 06:01:36 +0100

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -16,6 +16,9 @@ auth_type = getenv('AUTH_TYPE')
 if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 elif auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -28,17 +31,20 @@ def before_request():
         return
 
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/',
+                     '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
         abort(403)
+
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Handles Basic Authentication"""
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -32,3 +33,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns Current User"""
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request"""
+        if request is None:
+            return None
+            
+        session_name = getenv('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name)
